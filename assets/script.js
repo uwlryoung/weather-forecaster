3766c6bffb8ce31208ee9adbf032d31526cefc9c
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-06-25 22:30:17 -0400

    rework of forecast weather, creating elements onto the page instead of having it premade on html

--- assets/script.js
+++ assets/script.js
@@ -3,51 +3,105 @@ var currentWeather = $("#current-weather");
 var currentTemp = $("#temp");
 var currentWind = $("#wind");
 var currentHumidity = $("#humidity");
+var currentIcon = $("#icon");
 var currentCity = $("#city");
-var dayAfter1 = $("#dayAfter-1");
-var dayAfter2 = $("#dayAfter-2");
-var dayAfter3 = $("#dayAfter-3");
-var dayAfter4 = $("#dayAfter-4");
-var dayAfter5 = $("#dayAfter-5");
-
+var currentDate = $("#date");
+var displayCity = $("#displayCity");
+var futureForecast = document.getElementById("future-forecast");
 var searchFormEl = $("#search-form");
+
 var apiKey = "0abc1b8372c44587f42b9a4f3413df22";
 
-// var lat = 0;
-// var lon = 0;
 
-//Function to get the weather
+//Click Event to get the submitted city 
 searchFormEl.on("submit", function(event){
     event.preventDefault();
+    
     var city = currentCity.val();
-
-    var requestLatLon = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
-    fetch(requestLatLon)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-        // console.log(data);
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-        var requestWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
-
-        fetch(requestWeather)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-    });
+    displayCity.text(city);
+    getLatLon(city);
 });   
 
+//Functions to get the Current Weather (first gets the Latitude/Longitude, then gets the weather from the city)
+function getLatLon (city){
+    var requestURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+    fetch(requestURL)
+    .then(function (response){
+      return response.json();
+    })
+    .then(function (data){
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      getCurrentWeather(lat,lon);
+      getForecastWeather(lat,lon);
+    })
+  
+};
+
+function getCurrentWeather (lat,lon){
+  var requestURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+  fetch(requestURL)
+    .then(function (response){
+      return response.json();
+    })
+    .then(function (data){
+      console.log(data);
+      currentDate.text(dayjs().format("dddd[, ]MMMM[ ]D"));
+      currentIcon.attr("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+      currentTemp.text("Temp: " + data.main.temp + "\u00B0F");
+      currentWind.text("Wind: " + data.wind.speed + " MPH");
+      currentHumidity.text("Humidity: " + data.main.humidity + "%")
+    })
+};
+
+function getForecastWeather (lat,lon){
+  var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+  fetch(requestURL)
+    .then(function (response){
+      return response.json();
+    })
+    .then(function (data){
+      console.log(data);
+
+      var forecastHeader = document.createElement("h2");
+      forecastHeader.textContent = "5-day Forecast";
+      futureForecast.appendChild(forecastHeader);
 
 
+      for(i = 1; i < data.list.length; i+=8){
+        console.log(data.list[i].main.temp);
+        console.log(data.list[i].dt_txt);
 
+        var weatherCard = document.createElement("div");
+        weatherCard.setAttribute("class", "future-weather col");
 
+        futureForecast.appendChild(weatherCard);
 
+        var dateEl = document.createElement("p");
+        dateEl.style.fontSize = "20px";
+        dateEl.textContent = dayjs.unix(data.list[i].dt).format("ddd[,] MMM D")
+        weatherCard.appendChild(dateEl);
 
+        var iconEl = document.createElement("img");
+        iconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png");
+        weatherCard.appendChild(iconEl);
+
+        var tempEl = document.createElement("p");
+        tempEl.textContent = "Temp: " + data.list[i].main.temp + "\u00B0F";
+        weatherCard.appendChild(tempEl);
+
+        var windEl = document.createElement("p");
+        windEl.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
+        weatherCard.appendChild(windEl);
+
+        var humidityEl = document.createElement("p");
+        humidityEl.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+        weatherCard.appendChild(humidityEl);
+      }
+    })
+
+    
+};
 
 
 //TODO - A click function for when a city is searched. 
@@ -60,3 +114,4 @@ searchFormEl.on("submit", function(event){
 
 //TODO - A function that when a user clicks on one of the rendered history search buttons
 //that weather data is pulled up and rendered
+
